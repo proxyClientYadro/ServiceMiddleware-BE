ee8acc37d8e1db6314f725adf6761dcde31a8639
maksim.lazko <m.lazko@naveksoft.com>
2024-01-24 14:23:57 +0300

    refactored users/views file

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
 from typing import Any
 
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -16,17 +18,11 @@ class __BaseUserOperationView(APIView):
     endpoint: str = None
 
     @handle_json_decode_error
-    def __send_request(self, request: Request) -> Response:
+    def handle_request(self, request: Request) -> Response:
         response = Route(request=request).send(endpoint=self.endpoint)
         Logger().log_proxy_response_to_client(response=response)
         Logger().save_to_db()
         return response
-
-    def get(self, request: Request, *args: Any, **kwargs: dict) -> Response:
-        return self.__send_request(request=request)
-
-    def post(self, request: Request, *args: Any, **kwargs: dict) -> Response:
-        return self.__send_request(request=request)
 
 
 class UserRegistrationView(__BaseUserOperationView):
@@ -34,32 +30,55 @@ class UserRegistrationView(__BaseUserOperationView):
 
     endpoint = 'users'
 
+    def post(self, request: Request, *args: Any, **kwargs: dict) -> Response:
+        return self.handle_request(request=request)
+
 
 class LoginView(__BaseUserOperationView):
-    """Logins User in the third-party service"""
+    """Login User in the third-party service"""
 
     endpoint = 'users/login'
+    authentication_classes = (SessionAuthentication,)
+
+    def post(self, request: Request, *args: Any, **kwargs: dict) -> Response:
+        return self.handle_request(request=request)
 
 
 class LogoutView(__BaseUserOperationView):
-    """Logouts User in the third-party service"""
+    """Logout User in the third-party service"""
 
     endpoint = 'users/logout'
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request: Request, *args: Any, **kwargs: dict) -> Response:
+        return self.handle_request(request=request)
 
 
 class EmailVerificationCheckView(__BaseUserOperationView):
     """Checks Email Verification status"""
 
     endpoint = 'users/email-verification/check'
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request: Request, *args: Any, **kwargs: dict) -> Response:
+        return self.handle_request(request=request)
 
 
 class EmailVerificationResendView(__BaseUserOperationView):
     """Resends Email Verification"""
 
     endpoint = 'users/email-verification/resend'
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request: Request, *args: Any, **kwargs: dict) -> Response:
+        return self.handle_request(request=request)
 
 
 class EmailVerificationVerifyView(__BaseUserOperationView):
     """Verifies Email"""
 
     endpoint = 'users/email-verification/verify'
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request: Request, *args: Any, **kwargs: dict) -> Response:
+        return self.handle_request(request=request)
