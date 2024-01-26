6b7a629b68bb0479aeda6135d3281fe0cf737d4b
maksim.lazko <m.lazko@naveksoft.com>
2024-01-26 15:48:29 +0300

    user model, serializer, manager

---
+++ users/models.py
@@ -0,0 +1,24 @@
+import uuid
+
+from django.contrib.auth.models import AbstractBaseUser
+from django.db import models
+
+from users.managers import CustomUserManager
+
+
+class UserModel(AbstractBaseUser):
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    access_token = models.CharField(max_length=255, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self) -> str:
+        return f'{self.uuid} | {self.email} | {self.is_staff}'
